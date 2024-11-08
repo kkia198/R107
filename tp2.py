0f70413fa87d56bb7cd4607246b120c554951250
kkia198 <kianakamranikia2033@gmail.com>
2024-11-08 16:39:29 +0100

    test

---
+++ tp2.py
@@ -0,0 +1,13 @@
+x = int(input("entrez x : "))
+y = int(input("entrez y: "))
+
+print("avant permutation :")
+print("x :", x)
+print("y :", y)
+temps = x
+x = y
+y = temps
+
+print("aprés permutation:")
+print("x :", x)
+print("y :", y)
