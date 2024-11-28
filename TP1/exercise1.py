3ed57be5aa8ee63fc200bafaab2202319dc934b3
kkia198 <kianakamranikia2003@gmail.com>
2024-11-28 09:22:16 +0100

    test

---
+++ TP1/exercise1.py
@@ -0,0 +1,5 @@
+jour = int(input("rentrez le nombre de jours"))
+heure = int(input("rentrez le nombre de heurs"))
+minute = int(input("rentrez le nombre de minutes"))
+jour = jour*1440
+heure = heure*60
