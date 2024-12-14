225ff5ca12b571a6646d493ebb887fb9c416b88d
kkia198 <kianakamranikia2003@gmail.com>
2024-12-14 09:55:53 +0100

    test

---
+++ TP 4/ex1.py
@@ -0,0 +1,13 @@
+nombre = float(input("Vous cherchez la table de multiplication de quel nombre ? "))
+
+
+multilist = []
+
+for i in range(10):
+    multilist = nombre * i
+    multilist.append(round(multilist, 1))
+
+
+for i, multilist in enumerate(multilist):
+    print(f"{nombre} * {i} = {multilist}")
+test
